257786a982c986d4bf70ee438e2078e7e2dcab06
Marcelo Clivio <97415092+mclivio@users.noreply.github.com>
2023-02-25 16:35:21 -0300

    added palette, theme and fonts

--- app/src/main/java/com/donc/smt_utils/ui/theme/Type.kt
+++ app/src/main/java/com/donc/smt_utils/ui/theme/Type.kt
@@ -5,30 +5,91 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.donc.smt_utils.R
 
 // Set of Material typography styles to start with
+private val QuickSand = FontFamily(
+    Font(R.font.quicksand_light, FontWeight.W300),
+    Font(R.font.quicksand_regular, FontWeight.W400),
+    Font(R.font.quicksand_medium, FontWeight.W500),
+    Font(R.font.quicksand_bold, FontWeight.W600)
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    displayLarge = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W400,
+        fontSize = 57.sp,
+    ),
+    displayMedium = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W400,
+        fontSize = 45.sp,
+    ),
+    displaySmall = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W400,
+        fontSize = 36.sp,
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W500,
+        fontSize = 32.sp,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W400,
+        fontSize = 28.sp,
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W400,
+        fontSize = 24.sp,
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W400,
         fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W500,
+        fontSize = 16.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W400,
+        fontSize = 16.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W400,
+        fontSize = 14.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W400,
+        fontSize = 12.sp,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W500,
+        fontSize = 12.sp,
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+        fontFamily = QuickSand,
+        fontWeight = FontWeight.W500,
         fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
     )
-    */
 )
